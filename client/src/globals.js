a16d90a8239826f985d76c9ee2026c0ed753eeef
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-09-02 10:43:13 -0600

    added marvel api results homepage

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,6 +1,19 @@
+import md5 from 'md5'
+export const MARVEL_HASH = `bf82e8d1ba35c9151756f199bf3d63593344869f`
 export const MARVEL_KEY = '411f30e793bd61e8cd96ac17f835698d'
 export const BASE_URL = 'http://localhost:3001/api'
 export const MARVEL_URL = 'https://gateway.marvel.com'
 export const MARVEL_COMICS =
-  'https://gateway.marvel.com:443/v1/public/comics?title='
-export const MARVEL_ORDERBY = '&orderBy=focDate&apikey='
+  'http://gateway.marvel.com/v1/public/comics?titleStartsWith='
+export const SEARCH_BY_CHARACTER =
+  'http://gateway.marvel.com/v1/public/characters?name='
+export const TIMESTAMP = new Date().getTime()
+export const STRING_TO_HASH = TIMESTAMP + MARVEL_HASH + MARVEL_KEY
+export const HASH = md5(STRING_TO_HASH)
+
+export const MARVEL_ORDERBY = '&orderBy=focDate'
+
+export const COMICVINE_URL = 'http://comicvine.gamespot.com/api'
+export const COMICVINE_KEY = 'c55d019a55254a9ebd48ffa6bf63220facededb9'
+
+//credits https://www.titanwolf.org/Network/q/abce4c5d-da5f-4b90-af3e-d4055c838ffc/y
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from '../components/Searchcomics'
+import {
+  MARVEL_COMICS,
+  MARVEL_KEY,
+  MARVEL_ORDERBY,
+  MARVEL_HASH,
+  TIMESTAMP,
+  HASH,
+  SEARCH_BY_CHARACTER
+} from '../globals'
+import axios from 'axios'
 function Home(props) {
+  const [searchResults, setSearchResults] = useState([])
+  const [searched, toggleSearched] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
+  const getSearchResults = async (e) => {
+    try {
+      e.preventDefault()
+      const res = await axios.get(
+        `${MARVEL_COMICS}${searchQuery}&ts=${TIMESTAMP}&apikey=${MARVEL_KEY}&hash=${HASH}`
+      )
+      console.log(res)
+      setSearchResults(res.data.data.results)
+      toggleSearched(true)
+      setSearchQuery('')
+    } catch (error) {}
+  }
+
+  const getResults = (e) => {
+    setSearchQuery(e.target.value)
+  }
   return (
     <div>
       <h4>Search Comics</h4>
-      <Search />
+      <Search
+        value={searchQuery}
+        onChange={getResults}
+        onSubmit={getSearchResults}
+      />
       <br />
+      <div>
+        {searchResults.map((search) => (
+          <div key={search.title} className="comicCard">
+            <h3>{search.title}</h3>
+            <img
+              className="thumbnail"
+              src={`${search.thumbnail.path}/portrait_fantastic.jpg`}
+              alt={search.title}
+            />
+            <p>{search.description}</p>
+            <button>Add To Shelf</button>
+          </div>
+        ))}
+      </div>
       <h4>Random Comic Recommendation</h4>
     </div>
   )
