bc6464823f996d1ad4d970689b722a8c42d4ed65
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-09-03 20:04:45 -0600

    added more bootstrap features

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -8,6 +8,7 @@ import {
   BASE_URL
 } from '../globals'
 import axios from 'axios'
+import { Carousel, CarouselItem } from 'react-bootstrap'
 function Home(props) {
   const [searchResults, setSearchResults] = useState([])
   const [searched, toggleSearched] = useState(false)
@@ -40,6 +41,45 @@ function Home(props) {
         onChange={getResults}
         onSubmit={getSearchResults}
       />
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://img.freepik.com/free-vector/comic-style-wallpaper_79603-1248.jpg?size=626&ext=jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://2.bp.blogspot.com/-CcpAOigy-cw/UwCdkpuo6XI/AAAAAAAABJk/_nIoklxvAiE/s1600/marveluniverse.jpg"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://wallpapercave.com/wp/pDF7GZ6.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
       <div className="grid" background-color="darkgrey">
         {searchResults.map((search) => (
           <div key={search.title} className="comicCard">
